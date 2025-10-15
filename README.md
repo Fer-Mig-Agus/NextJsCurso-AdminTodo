a337c0d94354d9ca4f90a83fdb9b66e10013ca32
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2025-10-15 12:28:10 -0300

    Se agrego al relacion de uno a muchos entre user y todo

--- README.md
+++ README.md
@@ -8,6 +8,21 @@ docker compose up -p
 ```
 2. Renombrar el .env.template a .env
 3. Reemplazar las variables de entorno
+4. Ejecutar el comando ```npm install``` para construir los modulos de node
+5. Ejecutar el comando `npm run dev` para ejecutar aplicacion de desarrollo
+6. Ejecutar estos comandos de Prisma
+
+```
+npx prisma migrate dev
+npx prisma generate
+```
+
+7. Ejecutar el SEED para [crear la BDD local]('localhost:3000/api/seed')
+
+## Nota: Usuario por defecto
+
+default user= test1@google.com
+default password=123456
 
 # Prisma commands
 
--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,20 +1,31 @@
 import prisma from '@/lib/prisma';
+import bcrypt from 'bcryptjs';
 import { NextResponse, NextRequest } from 'next/server'
 
 export async function GET(request: Request) {
 
   await prisma.todo.deleteMany();
+  await prisma.user.deleteMany();
 
-  await prisma.todo.createMany({
-    data:[
+
+  const user = await prisma.user.create({
+    data:{
+      email:'test1@google.com',
+      password: bcrypt.hashSync('123456'),
+      roles: ['admin','client','super-user'],
+      todos:{
+        create:[
       {description: 'Piedra del alma', complete:true},
       {description: 'Piedra del poder'},
       {description: 'Piedra del tiempo'},
       {description: 'Piedra del espacio'},
-      {description: 'Piedra del realidad'},
-
-    ]
+      {description: 'Piedra del realidad', complete:true},
+        ]
+      }
+    }
   })
+
+
 
   // const todo = await prisma.todo.create({
   //   data: {
